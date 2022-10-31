ca846e4192338a17288c854c6d9c20ad5396143e
Quintilian Huynh <megaxq@gmail.com>
2022-10-30 18:14:33 -0700

    Fun with Javascript
    CSC196 Assn4

---
+++ fancifymytext.js
@@ -0,0 +1,39 @@
+
+
+function bigFunction() {
+
+    document.getElementById("textbox").style.fontSize = "32px";
+        
+    }
+
+function fancyFunction() {
+
+    document.getElementById("textbox").style.fontWeight = 'bold';
+    document.getElementById("textbox").style.color = 'blue';
+    document.getElementById("textbox").style.textDecoration = "underline";
+        
+    }
+    
+    
+function boringFunction() {
+
+    document.getElementById("textbox").style.fontWeight = null;
+    document.getElementById("textbox").style.color = null;
+    document.getElementById("textbox").style.textDecoration = null;
+    document.getElementById("textbox").style.fontSize = null; 
+    }
+
+    function mooFunction() {
+
+        var str = document.getElementById("textbox").value;
+        var parts = str.split(".");  // ["How", "are", "you?"]
+        str = parts.join("moo.");       // "How_are_you?"
+
+
+        document.getElementById("textbox").value = document.getElementById("textbox").value.toUpperCase();
+        document.getElementById("textbox").value = str;
+        
+     }
+
+   
+
